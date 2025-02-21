5422288027b14b4e6a7a7815288f98b33b7c28b2
Matias Perez <43894343+matiasperz@users.noreply.github.com>
2025-02-21 18:55:54 -0300

    Initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig([
+  /* Default core entrypoint - Add your own entrypoints here */
+  {
+    entry: ['packages/core/index.ts'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    treeshake: true,
+    minify: true,
+  },
+])
