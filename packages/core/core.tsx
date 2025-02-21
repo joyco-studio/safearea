d1c7cc93396c9e82c2962713731487b8ed212f73
Matias Perez <matiasperezpc@hotmail.com>
2025-02-21 19:09:24 -0300

    init

--- packages/core/core.tsx
+++ packages/core/core.tsx
@@ -1,7 +1,53 @@
-export const GenericComponent = () => {
-  return <div>GenericComponent</div>
+/* eslint-disable react-compiler/react-compiler */
+import { RefObject, useMemo, useRef } from 'react'
+import useMeasure from 'use-measure'
+
+const BoundsComponent = (ref: RefObject<HTMLDivElement>) => {
+  return ({ debug = false, style, ...props }: React.HTMLAttributes<HTMLDivElement> & { debug?: boolean }) => {
+    const { visibility } = style || {}
+
+    if (visibility) {
+      console.warn('BoundsArea: style properties override. Avoid passing visibility property.')
+    }
+
+    return (
+      <div
+        {...props}
+        style={{
+          visibility: debug ? 'visible' : 'hidden',
+          background: debug ? 'rgba(255, 0, 0, 0.25)' : undefined,
+          ...style,
+        }}
+        ref={ref}
+      />
+    )
+  }
 }
 
-export const useGenericHook = () => {
-  return 'GenericHook'
+export const useContainedBounds = (dimensions: { width: number; height: number }) => {
+  const boundsRef = useRef<HTMLDivElement>(null)
+  const bounds = useMeasure(boundsRef)
+
+  const mediaAspectRatio = dimensions.width / dimensions.height
+  const boundsAspectRatio = bounds.width / bounds.height
+
+  let resolvedWidth, resolvedHeight
+
+  if (mediaAspectRatio > boundsAspectRatio) {
+    // Scale by width
+    resolvedWidth = bounds.width
+    resolvedHeight = (bounds.width / dimensions.width) * dimensions.height
+  } else {
+    // Scale by height
+    resolvedHeight = bounds.height
+    resolvedWidth = (bounds.height / dimensions.height) * dimensions.width
+  }
+
+  const BoundsArea = useMemo(() => BoundsComponent(boundsRef), [])
+
+  return {
+    BoundsArea,
+    containedWidth: resolvedWidth,
+    containedHeight: resolvedHeight,
+  }
 }
